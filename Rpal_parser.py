3a7b82d29ebdc87afd1e6c31a286d5b1e1797e9d
LasonRx360 <2001lasanasubasinghe@gmail.com>
2024-05-17 15:55:45 +0530

    recursion bug

--- Rpal_parser.py
+++ Rpal_parser.py
@@ -383,14 +383,14 @@ def Vl():
     if count>0:
         s.build_tree(',', count+1)
 
-with open('tests/test_2') as file:
+with open('tests/test_5') as file:
     file_content = file.read()
     
 file.close()
 l = lexicalAnalyzer.sendCharactersToLex(file_content)
 
 E()
-ast = stack.tree(s.nodeList[0])
+# ast = stack.tree(s.nodeList[0])
 
 # stack.preOrderTraversal(ast.head)
 
@@ -398,18 +398,14 @@ standardTree = s.nodeList[0].standardizeNode()
 
 st = stack.tree(standardTree)
 
-print('standard tree')
-
-# stack.preOrderTraversal(st.head)
+stack.preOrderTraversal(st.head)
 
 usable = stack.usableStandardizedTree(st.head)
 
 stack.preOrderTraversalUsable(usable)
 
-cseMachine.generateContrlStruct(st.head,0)
+cseMachine.generateContrlStruct(usable,0)
 
-print(cseMachine.controlStructs)
-# CSEmachine.flattenStandardTree(st.head)
+# print(cseMachine.controlStructs)
 
-# print(CSEmachine.appExp)
 
